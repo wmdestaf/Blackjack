8cce52a07fe6853ce573f33a97d61baeeb39367d
Will DeStaffan <w55@umbc.edu>
2022-05-04 01:02:12 -0400

    C code. needs gmp added

--- algo/pollard_rho_python.py
+++ algo/pollard_rho_python.py
@@ -1,15 +1,16 @@
 from math import gcd
 from concurrent.futures import ProcessPoolExecutor
 from time import time, sleep
+from random import randint
 
 FAILURE = -1
-def g(x,n):
+def g(x,c,n):
     return ((x*x)+1) % n
     
 def pollard_rho_parallel_naive(n, threads, start=2):
     #start 'thread' threads from 2 to 2+threads
     with ProcessPoolExecutor(max_workers=threads) as executor:
-        futures = [executor.submit(pollard_rho, n, start + i, False) for i in range(threads)]
+        futures = [executor.submit(pollard_rho, n, start + i, None, False) for i in range(threads)]
         i = start + threads
         while True:
             sleep(2)
@@ -20,7 +21,7 @@ def pollard_rho_parallel_naive(n, threads, start=2):
                 futures.remove(future)
                 
                 if res == -1: #fail, retry.
-                    futures.append(executor.submit(pollard_rho, n, start + i, False))
+                    futures.append(executor.submit(pollard_rho, n, start + i, None, False))
                     i += 1
                 else: #success, or is_prime
                     for future in futures:
@@ -28,14 +29,17 @@ def pollard_rho_parallel_naive(n, threads, start=2):
                     return res
     
     
-def pollard_rho(n,start=2,retry_on_fail=True):
+def pollard_rho(n,start=2,c=1, retry_on_fail=True):
     x = start
     y = start
     d = 1
+    
+    if c == None:
+        c = randint(1,255)
 
     while d == 1:
-        x = g(x,n)
-        y = g(g(y,n),n)
+        x = g(x,c,n)
+        y = g(g(y,c,n),c,n)
         d = gcd(abs(x - y), n)
         
     if start == n: #prime!!
@@ -50,12 +54,12 @@ def pollard_rho(n,start=2,retry_on_fail=True):
         return d
 
 if __name__ == "__main__":
-    n = 410082333130306064848248112591
+    n = 98834976202698839303077
     #n=17
     serial = True
     
     old = time()
-    print(pollard_rho_parallel_naive(n,2,start=2))
+    print(pollard_rho_parallel_naive(n,1,start=2))
     new = time()
     print("Parallel: %g" % (new - old))
     
